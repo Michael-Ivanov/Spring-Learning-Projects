46f43f3eb9d85ae691ca8f47bf5375ca2ea7b508
Michael Ivanov <eesseth@gmail.com>
2021-01-05 23:13:25 +0300

    Add SwimCoach bean created without component scanning

--- SpringDemoAnnotations/src/main/java/springdemo/SwimJavaConfigDemoApp.java
+++ SpringDemoAnnotations/src/main/java/springdemo/SwimJavaConfigDemoApp.java
@@ -4,15 +4,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import springdemo.beans.Coach;
 import springdemo.config.SportConfig;
 
-public class JavaConfigDemoApp {
+public class SwimJavaConfigDemoApp {
 
     public static void main(String[] args) {
 
         // read spring config (java config source code)
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SportConfig.class);
 
         // get the bean from spring container
-        Coach coach = context.getBean("tennisCoach", Coach.class);
+        Coach coach = context.getBean("swimCoach", Coach.class);
 
         // call a method on the bean
         System.out.println(coach.getDailyWorkout());
--- SpringDemoAnnotations/src/main/java/springdemo/beans/SwimCoach.java
+++ SpringDemoAnnotations/src/main/java/springdemo/beans/SwimCoach.java
@@ -1,29 +1,16 @@
 package springdemo.beans;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-@Component
 public class SwimCoach implements Coach {
 
-    @Autowired
-    @Qualifier("happyFortuneService")
     private FortuneService fortuneService;
 
-    public SwimCoach() {
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
     }
-
-//    @Autowired
-//    public SwimCoach(FortuneService fortuneService) {
-//        System.out.println("Inside the parameter constructor method");
-//        System.out.println("Incoming FortuneService is: " + fortuneService.getFortune());
-//        this.fortuneService = fortuneService;
-//    }
 
     @Override
     public String getDailyWorkout() {
-        return "Swim 5 km right now";
+        return "Swim 1000 meters as a warm up";
     }
 
     @Override
@@ -31,15 +18,4 @@ public class SwimCoach implements Coach {
         return fortuneService.getFortune();
     }
 
-//    @Autowired
-//    public void setFortuneService(FortuneService fortuneService) {
-//        System.out.println("Inside the setter method");
-//        this.fortuneService = fortuneService;
-//    }
-
-//    @Autowired
-//    public void doSomeCrazyStuff(FortuneService fortuneService) {
-//        System.out.println("Inside the crazy method");
-//        this.fortuneService = fortuneService;
-//    }
 }
