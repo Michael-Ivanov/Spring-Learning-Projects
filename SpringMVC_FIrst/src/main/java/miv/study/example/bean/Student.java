79665938df1db43061c543dd69a227064005bf1a
Michael Ivanov <eesseth@gmail.com>
2021-01-13 19:58:35 +0300

    Update: drop-down list gets values from another class List using just getter

--- SpringMVC_FIrst/src/main/java/miv/study/example/bean/Student.java
+++ SpringMVC_FIrst/src/main/java/miv/study/example/bean/Student.java
@@ -1,25 +1,13 @@
 package miv.study.example.bean;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
-
-    private LinkedHashMap<String, String> countryOptions;
-
-    public Student() {
-        // populate country options: used ISO country code
-        countryOptions = new LinkedHashMap<>();
-
-        countryOptions.put("BR", "Brazil");
-        countryOptions.put("FR", "France");
-        countryOptions.put("DE", "Germany");
-        countryOptions.put("IN", "India");
-        countryOptions.put("RU", "Russia");
-    }
 
     public String getFirstName() {
         return firstName;
@@ -45,8 +33,8 @@ public class Student {
         this.country = country;
     }
 
-    public LinkedHashMap<String, String> getCountryOptions() {
-        return countryOptions;
+    public LinkedList<String> getCountryOptions() {
+        return Countries.getCountriesList();
     }
 
 }
