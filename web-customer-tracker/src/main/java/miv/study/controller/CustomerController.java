2cf9aea9728e525d72ea001477194fa4d1595366
Michael Ivanov <eesseth@gmail.com>
2021-02-04 22:13:27 +0300

    Add: web-customer-tracker project get customers list from database and show on web page

--- web-customer-tracker/src/main/java/miv/study/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/miv/study/controller/CustomerController.java
@@ -1,15 +1,29 @@
 package miv.study.controller;
 
+import miv.study.dao.CustomerDAO;
+import miv.study.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // need to inject the customer dao
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model model) {
+
+        // get customer from the dao
+        List<Customer> customers = customerDAO.getCustomers();
+        // add the customers to the model
+        model.addAttribute("customers", customers);
 
         return "list-customers";
     }
---
+++ web-customer-tracker/src/main/java/miv/study/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package miv.study.dao;
+
+import miv.study.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    List<Customer> getCustomers();
+}
