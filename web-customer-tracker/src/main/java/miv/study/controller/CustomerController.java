7b30a667bee666754b7345f0387c2f6570ea273e
Michael Ivanov <eesseth@gmail.com>
2021-02-07 20:11:08 +0300

    Add: update customer functionality

--- web-customer-tracker/src/main/java/miv/study/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/miv/study/controller/CustomerController.java
@@ -3,13 +3,11 @@ package miv.study.controller;
 import miv.study.dao.CustomerDAO;
 import miv.study.entity.Customer;
 import miv.study.service.CustomerService;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +36,16 @@ public class CustomerController {
         Customer customer = new Customer();
         model.addAttribute("customer", customer);
 
+        return "customer-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("customerId") int id, Model model) {
+        // get the customer from the service
+        Customer customer = customerService.getCustomer(id);
+        // set customer as a model attribute to pre-populate the form
+        model.addAttribute("customer", customer);
+        // send over to our form
         return "customer-form";
     }
 
