be6bf28edb5d3e66fc81e7fcc5db435c9c048179
Michael Ivanov <eesseth@gmail.com>
2021-02-08 21:53:16 +0300

    Add: pointcut expression matches two different methods example

--- spring-demo-aop/src/main/java/miv/study/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/main/java/miv/study/aopdemo/MainDemoApp.java
@@ -2,6 +2,7 @@ package miv.study.aopdemo;
 
 import miv.study.aopdemo.config.DemoConfig;
 import miv.study.aopdemo.dao.AccountDAO;
+import miv.study.aopdemo.dao.MembershipDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainDemoApp {
@@ -12,15 +13,13 @@ public class MainDemoApp {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(DemoConfig.class);
 
-        // get the bean from spring container
+        // get the beans from spring container
         AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+        MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 
         // call the business logic
         accountDAO.addAccount();
-
-        System.out.println("let's call it again");
-
-        accountDAO.addAccount();
+        membershipDAO.addAccount();
 
         // close the context
         context.close();
