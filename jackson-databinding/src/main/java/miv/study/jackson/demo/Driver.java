9234e976855c8aeb1b8ebe37f93f774cbebb31ec
Michael Ivanov <eesseth@gmail.com>
2021-02-17 23:06:14 +0300

    Change: add fields to Student.class to process sample-full.json

--- jackson-databinding/src/main/java/miv/study/jackson/demo/Driver.java
+++ jackson-databinding/src/main/java/miv/study/jackson/demo/Driver.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Driver {
 
@@ -15,11 +16,17 @@ public class Driver {
 
             // read JSON file and map/convert to Java POJO
             Student student = mapper.readValue(
-                    new File("data/sample-lite.json"), Student.class);
+                    new File("data/sample-full.json"), Student.class);
 
             // print first name and last name
             System.out.println("First name: " + student.getFirstName());
             System.out.println("Last name: " + student.getLastName());
+            // print out address
+            Address address = student.getAddress();
+            System.out.println("Street: " + address.getStreet());
+            System.out.println("City: " + address.getCity());
+            // print out languages
+            System.out.println(Arrays.toString(student.getLanguages()));
         } catch (IOException e) {
             e.printStackTrace();
         }
