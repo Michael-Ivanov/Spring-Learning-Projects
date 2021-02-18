76b89644454145266aad21f1fb773f68bffa5d89
Michael Ivanov <eesseth@gmail.com>
2021-02-18 22:14:07 +0300

    Add: @PathVariable argument injection returning student at index

--- rest-controller-demo/src/main/java/miv/study/rest/StudentRestController.java
+++ rest-controller-demo/src/main/java/miv/study/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package miv.study.rest;
 
 import miv.study.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> studentList = new ArrayList<>();
+    private List<Student> studentList;
+
+    @PostConstruct
+    public void loadData() {
+        studentList = new ArrayList<>();
         studentList.add(new Student("John", "Malkovich"));
         studentList.add(new Student("John", "Watson"));
         studentList.add(new Student("John", "McClane"));
         studentList.add(new Student("John", "Bon Jovi"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return studentList;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return studentList.get(studentId);
     }
 }
