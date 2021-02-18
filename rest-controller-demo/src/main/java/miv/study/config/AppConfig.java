266fe6ba486417a3cbd31a5d9fc8a430fa8830c2
Michael Ivanov <eesseth@gmail.com>
2021-02-18 20:21:24 +0300

    New Spring REST controller project basic All-Java configuration

---
+++ rest-controller-demo/src/main/java/miv/study/config/AppConfig.java
@@ -0,0 +1,11 @@
+package miv.study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("miv.study")
+public class AppConfig {
+}
