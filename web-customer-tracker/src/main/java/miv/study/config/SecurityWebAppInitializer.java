55710ddcc6f61a928b89b3a463b2c8f9386eb8ce
Michael Ivanov <eesseth@gmail.com>
2021-02-21 23:15:25 +0300

    Add: Spring-Security support for REST API

---
+++ web-customer-tracker/src/main/java/miv/study/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package miv.study.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
