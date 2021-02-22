0eb87bb995601de7ef90d2199d3e440dbc395452
Michael Ivanov <eesseth@gmail.com>
2021-02-22 20:19:32 +0300

    Add new Spring Boot Web starter

---
+++ mycoolapp/src/main/java/miv/study/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package miv.study.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+}
