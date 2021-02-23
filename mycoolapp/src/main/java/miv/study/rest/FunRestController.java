a1dbac78bc6176ace42faf3164af975a3dcfa711
Michael Ivanov <eesseth@gmail.com>
2021-02-23 21:54:15 +0300

    New project: cruddemo - REST API on Spring Boot

--- mycoolapp/src/main/java/miv/study/rest/FunRestController.java
+++ mycoolapp/src/main/java/miv/study/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package miv.study.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,17 @@ import java.time.LocalDateTime;
 @RestController
 public class FunRestController {
 
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${coach.team}")
+    private String coachTeam;
+
     @GetMapping("/")
     public String getSomething() {
 
-        return "Hello World! Time on server is " + LocalDateTime.now();
+        return "Hello World! Time on server is " + LocalDateTime.now() +
+                "<br> Coach Name is " + coachName +
+                "<br> Coach Team is " + coachTeam;
     }
 
     // expose a new endpoint
